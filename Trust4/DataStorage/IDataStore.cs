f1a0ed724e7945022ca2226bdc41738bdee8457a
martindevans <martindevans@localhost>
2010-12-03 07:39:40 +0000

    adding data store stuff

---
+++ Trust4/DataStorage/IDataStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DistributedServiceProvider.Base;
+using DistributedServiceProvider.Contacts;
+
+namespace Trust4.DataStorage
+{
+    public interface IDataStore
+    {
+        void Put(Identifier512 key, byte[] value);
+
+        void Delete(Identifier512 key);
+
+        IEnumerable<DataResult> Get(Identifier512 key);
+    }
+}
