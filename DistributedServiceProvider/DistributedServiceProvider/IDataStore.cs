0e6f6cd750bb5eb0c03708c1895d2d93a4bb80d1
James Rhodes <James Rhodes@localhost>
2010-12-03 18:03:49 +1100

    Implemented IDataStore and NullDataStore. Updated DnsProcess to use the new data store mechanism in the DHT.

---
+++ DistributedServiceProvider/DistributedServiceProvider/IDataStore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DistributedServiceProvider.Contacts;
+using DistributedServiceProvider.Base;
+using DistributedServiceProvider.MessageConsumers;
+
+namespace DistributedServiceProvider
+{
+    /// <summary>
+    /// An interface representing a data store.
+    /// </summary>
+    public interface IDataStore
+    {
+        /// <summary>
+        /// Puts a key-value pair into the local data store that's participating in the DHT.
+        /// </summary>
+        /// <param name="key">The unique key.</param>
+        /// <param name="value">The value.</param>
+        void Put(Identifier512 key, byte[] value);
+
+        /// <summary>
+        /// Returns a list of values retrieved from all of the connected peers for the selected
+        /// key request.
+        /// </summary>
+        /// <param name="key">The unique key.</param>
+        /// <param name="timeout">The timeout value for each peer.</param>
+        IEnumerable<KeyValuePair<Contact, byte[]>> Get(Identifier512 key, int timeout);
+    }
+}
