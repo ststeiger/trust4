82f288bc7b8f19485fe98a51732b5db2b97564b9
James Rhodes <James Rhodes@localhost>
2010-12-05 10:04:58 +1100

    Added support for MX records. Reformatted code, added licensing headers, remove and sorted using statements using MonoDevelop. Added missing files. Added .hgignore file.

--- Trust4/Program.cs
+++ Trust4/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ARSoft.Tools.Net.Dns;
-using System.Net;
-using System.Net.Sockets;
-using System.IO;
-using System.Threading;
-using DistributedServiceProvider.Base;
-using DistributedServiceProvider.Contacts;
-using DistributedServiceProvider;
+﻿// 
+//  Copyright 2010  Trust4 Developers
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
 
 namespace Trust4
 {
@@ -24,10 +26,7 @@ namespace Trust4
 
         public static Manager Manager
         {
-            get
-            {
-                return Program.p_Manager;
-            }
+            get { return Program.p_Manager; }
         }
     }
 }
