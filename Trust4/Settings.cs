4361e8ca5263b789e8ee695b5f03cfbd8182a0ff
martindevans <martindevans@localhost>
2010-12-04 18:19:54 +0000

    Nodes generate an identifier based off their public key, given the public key (eg.from a .key record) it's feasible to locate a node. Currently nodes are not challenged to prove that they possess the private key for the public key they're advertising, that's next. This allows constant pseudonymous identities within the network

--- Trust4/Settings.cs
+++ Trust4/Settings.cs
@@ -122,8 +122,7 @@ namespace Trust4
                         keySize = Int32.Parse(line[1]);
                         break;
                     case "routingidentifier":
-                        var s = line[1].Split(',');
-                        this.p_RoutingIdentifier = new Identifier512(new Guid(s[0]), new Guid(s[1]), new Guid(s[2]), new Guid(s[3]));
+                        Console.WriteLine("routingidentifier setting is deprecated and ignored");
                         break;
                     case "uid":
                         this.p_UnixUID = Convert.ToUInt32(value);
@@ -139,7 +138,8 @@ namespace Trust4
                 }
             }
 
-            CryptoProvider = LoadRsaKey(keySize);
+            CryptoProvider = LoadRsaKey(keySize, false);
+            p_RoutingIdentifier = Identifier512.CreateKey(CryptoProvider);
 			
 			if (Environment.OSVersion.Platform == PlatformID.Unix && (!setuid || !setgid))
 			{
@@ -147,11 +147,11 @@ namespace Trust4
 			}
         }
 
-        private RSACryptoServiceProvider LoadRsaKey(int keySize)
+        private RSACryptoServiceProvider LoadRsaKey(int keySize, bool forceNewKey)
         {
             RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(keySize);
 
-            if (File.Exists(PRIVATE_KEY_FILE_PATH) && File.Exists(PUBLIC_KEY_FILE_PATH))
+            if (File.Exists(PRIVATE_KEY_FILE_PATH) && File.Exists(PUBLIC_KEY_FILE_PATH) && !forceNewKey)
             {
                 string xml = File.ReadAllText(PRIVATE_KEY_FILE_PATH);
 
