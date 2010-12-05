e0813d1f76283e5001a25d3c1bb48058843cd733
martindevans <martindevans@localhost>
2010-12-05 03:44:19 +0000

    removed virtual from method headers in IDataStore.cs

--- Trust4/DataStorage/IDataStore.cs
+++ Trust4/DataStorage/IDataStore.cs
@@ -26,13 +26,13 @@ namespace Trust4.DataStorage
         /// </summary>
         /// <param name="key">The unique key.</param>
         /// <param name="value">The value.</param>
-        virtual void Put(Identifier512 key, byte[] value);
+        void Put(Identifier512 key, byte[] value);
 
         /// <summary>
         /// Deletes a key-value pair from the local data store that's participating in the DHT.
         /// </summary>
         /// <param name="key">The unique key.</param>
-        virtual void Delete(Identifier512 key);
+        void Delete(Identifier512 key);
 
         /// <summary>
         /// Returns a list of values retrieved from all of the connected peers for the selected
@@ -40,6 +40,6 @@ namespace Trust4.DataStorage
         /// </summary>
         /// <param name="key">The unique key.</param>
         /// <param name="timeout">The timeout value for each peer.</param>
-        virtual IEnumerable<DataResult> Get(Identifier512 key);
+        IEnumerable<DataResult> Get(Identifier512 key);
     }
 }
